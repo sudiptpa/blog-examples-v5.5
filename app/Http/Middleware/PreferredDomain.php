c9a2c41104881fa09617e486f0ce95e3d3b492d1
Sujip Thapa <sudiptpa@gmail.com>
2017-09-08 19:53:59 +0545

    Added laravel v5.5 files

---
+++ app/Http/Middleware/PreferredDomain.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class PreferredDomain
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Perform action
+
+        return $next($request);
+    }
+}
